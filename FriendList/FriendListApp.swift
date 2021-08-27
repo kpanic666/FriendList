0ef40fff2382c53789ff224ae483af2f90dfc21c
Andrei Korikov <kpanic666@gmail.com>
2021-08-27 13:45:53 +0500

    Initial Commit

---
+++ FriendList/FriendListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendListApp.swift
+//  FriendList
+//
+//  Created by Andrei Korikov on 27.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct FriendListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
