ea1a9f6c1e5ec07cf0f04a04156c9d969893f3b9
Andrei Korikov <kpanic666@gmail.com>
2021-08-27 22:20:35 +0500

    JSON parsing worked version

--- FriendList/ContentView.swift
+++ FriendList/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @ObservedObject var userContainer = UserContainer()
+    @State private var users = [User]()
     
     @State private var loadURL = "https://www.hackingwithswift.com/samples/friendface.json"
     
@@ -16,7 +16,7 @@ struct ContentView: View {
         NavigationView {
             VStack {
                 List {
-                    ForEach(userContainer.users) {
+                    ForEach(users) {
                         Text($0.name)
                     }
                 }
@@ -25,7 +25,7 @@ struct ContentView: View {
                 
                 HStack {
                     Button("Reload from Internet") {
-                        do { try userContainer.loadDataFromURL(string: loadURL)
+                        do { try loadDataFromURL(string: loadURL)
                         } catch {
                             print("Can't load user data from provided URL.")
                         }
@@ -34,6 +34,32 @@ struct ContentView: View {
             }
             .navigationBarTitle("My Friends")
         }
+    }
+    
+    /// Renew data in the 'users' array from provided URL
+    func loadDataFromURL(string: String) throws {
+        guard let url = URL(string: string) else {
+            print("Can't load Users from the Internet. Invalid ULR.")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                do {
+                    let decodedResponse = try JSONDecoder().decode([User].self, from: data)
+                    DispatchQueue.main.async {
+                        self.users = decodedResponse
+                    }
+                } catch {
+                    print(error.localizedDescription)
+                }
+                 
+            } else {
+                print("Can't load Users from the Internet. Fetch Failed.")
+            }
+        }.resume()
     }
 }
 
--- FriendList/User.swift
+++ FriendList/User.swift
@@ -16,74 +16,22 @@ struct User: Codable, Identifiable {
     let id: UUID
     var isActive = true
     var name: String
-    var age: Int = 20
-    var company = ""
-    var email = ""
-    var address = ""
-    var about = ""
-    let registered: Date
-    var tags = [String]()
-    var friends = [Friend]()
+//    var age: Int = 20
+//    var company = ""
+//    var email = ""
+//    var address = ""
+//    var about = ""
+//    let registered: Date
+//    var tags = [String]()
+//    var friends = [Friend]()
     
     init(id: UUID, name: String, regDate: Date) {
         self.id = id
         self.name = name
-        self.registered = regDate
+//        self.registered = regDate
     }
     
     init(name: String) {
         self.init(id: UUID(), name: name, regDate: Date())
-    }
-}
-
-class UserContainer: ObservableObject, Codable {
-    enum CodingKeys: CodingKey {
-        case users
-    }
-    
-    @Published var users: [User]
-    
-    init(users: [User]) {
-        self.users = users
-    }
-    
-    init() {
-        users = [User]()
-    }
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        users = try container.decode([User].self, forKey: .users)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(users, forKey: .users)
-    }
-    
-    /// Renew data in the 'users' array from provided URL
-    func loadDataFromURL(string: String) throws {
-        guard let url = URL(string: string) else {
-            print("Can't load Users from the Internet. Invalid ULR.")
-            return
-        }
-        
-        let request = URLRequest(url: url)
-        
-        URLSession.shared.dataTask(with: request) { data, response, error in
-            if let data = data {
-                do {
-                    let decodedResponse = try JSONDecoder().decode([User].self, from: data)
-                    DispatchQueue.main.async {
-                        self.users = decodedResponse
-                    }
-                } catch {
-                    print(error.localizedDescription)
-                }
-                 
-            } else {
-                print("Can't load Users from the Internet. Fetch Failed.")
-            }
-        }.resume()
     }
 }
